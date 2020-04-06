c136008ca9601026c1faeebaff1c925cf4975534
abenam27 <ga_benamara@esi.dz>
2020-04-06 14:35:42 +0200

    add exercise entity

---
+++ parson/src/Entity/Exercise.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ExerciseRepository")
+ */
+class Exercise
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $mark;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $solution = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(string $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getMark(): ?float
+    {
+        return $this->mark;
+    }
+
+    public function setMark(float $mark): self
+    {
+        $this->mark = $mark;
+
+        return $this;
+    }
+
+    public function getSolution(): ?array
+    {
+        return $this->solution;
+    }
+
+    public function setSolution(array $solution): self
+    {
+        $this->solution = $solution;
+
+        return $this;
+    }
+}
