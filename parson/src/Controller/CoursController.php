d07cc23d4623c1454a606b05765f4b21e72fed5c
abenam27 <ga_benamara@esi.dz>
2020-04-08 21:07:44 +0200

    add course form

--- parson/src/Controller/CoursController.php
+++ parson/src/Controller/CoursController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\CourseType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CoursController extends AbstractController
 {
     /**
-     * @Route("/cours", name="cours")
+     * @Route("/cours", name="course")
      */
     public function index()
     {
@@ -18,4 +19,20 @@ class CoursController extends AbstractController
             'controller_name' => 'CoursController',
         ]);
     }
+
+
+    /**
+     * @Route("cours/new",name="new_course")
+     */
+    public function new()
+    {
+        $form=$this->createForm(CourseType::class);
+
+        return $this->render('cours/new.html.twig',[
+            'coursForm'=>$form->createView()
+        ]);
+    }
+
+
+
 }
---
+++ parson/src/Form/CourseType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Course;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('imgUrl')
+            ->add('category')
+            ->add('timeNeeded')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Course::class,
+        ]);
+    }
+}
