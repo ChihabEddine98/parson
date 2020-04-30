0c449b08ccf70c56ed1febfa0f2165e48bc5be2b
abenam27 <ga_benamara@esi.dz>
2020-04-30 20:54:53 +0200

    update login stuff in controlle

--- parson/src/Controller/AuthController.php
+++ parson/src/Controller/AuthController.php
@@ -4,16 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils)
     {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('auth/login.html.twig', [
-            'controller_name' => 'AuthController',
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ]);
     }
 
