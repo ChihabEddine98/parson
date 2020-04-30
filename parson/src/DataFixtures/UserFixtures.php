127c2195ce35410536d8273d436376c6d413a2b7
abenam27 <ga_benamara@esi.dz>
2020-05-01 01:07:41 +0200

    hashed_password !

--- parson/src/DataFixtures/UserFixtures.php
+++ parson/src/DataFixtures/UserFixtures.php
@@ -4,9 +4,21 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends BaseFixture
 {
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+
+        $this->encoder = $encoder;
+    }
 
     protected function loadData(ObjectManager $manager)
     {
@@ -16,7 +28,7 @@ class UserFixtures extends BaseFixture
             $user->setEmail('user'.$i.'@mail.com')
                 ->setAddress($this->faker->streetAddress)
                 ->setPhone($this->faker->e164PhoneNumber)
-                -> setPassword("123")
+                ->setPassword($this->encoder->encodePassword($user,'123'))
                 ->setFullName($this->faker->name($sexe_text))
                 ->setSexe($sexe)
                 ->setCreatedAt($this->faker->dateTimeBetween('-1 months','-1 seconds'));
