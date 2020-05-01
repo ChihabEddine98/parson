fbf2e568bd586bf62166fd961de1ba81a9be7df0
abenam27 <ga_benamara@esi.dz>
2020-05-02 01:13:23 +0200

    Calcul de moyenne dans profile plus efficace (#2)

--- parson/src/Controller/UserController.php
+++ parson/src/Controller/UserController.php
@@ -15,26 +15,26 @@ class UserController extends AbstractController
      */
     public function index(UserRepository $repo)
     {
-        $users=$repo->findAll();
+        $users = $repo->findAll();
 
         return $this->render('user/user_list.html.twig', [
             'controller_name' => 'UserController',
-             'users'=> $users
+            'users' => $users
         ]);
     }
 
     /**
      * @Route("/users/{id}", name="user_profile")
      */
-    public function profile(User $user,UserCourseRepository $repoScore)
+    public function profile(User $user, UserCourseRepository $repoScore)
     {
         // Calculate avergae Rating !
-        $courses=$user->getCreatedCourses();
-        $somme=0;
-        $noteSomme=0;
-        $heureSomme=0;
-        $userSomme=0;
-        $i=0;
+        $courses = $user->getCreatedCourses();
+        $somme = 0;
+        $noteSomme = 0;
+        $heureSomme = 0;
+        $userSomme = 0;
+        $i = 0;
 //        foreach ($courses as $course)
 //        {
 //           $rate=$repoScore->findAverageRateByCourse($course);
@@ -63,16 +63,16 @@ class UserController extends AbstractController
 //             $moyenne=0;
 //         }
 
-        $avisMoyen=$repoScore->findAverageRateByUser($user);
+        $avisMoyen = $repoScore->findAverageRateByUser($user);
         $moyenne = $repoScore->findAverageByUser($user);
 
         return $this->render('user/user_profile.html.twig', [
             'controller_name' => 'UserController',
-            'user'=>$user,
-            'rating'=> $avisMoyen,
-            'average'=> $moyenne,
-            'nbUsers'=> $userSomme,
-            'nbHours'=> $heureSomme
+            'user' => $user,
+            'rating' => $avisMoyen,
+            'average' => $moyenne,
+            'nbUsers' => $userSomme,
+            'nbHours' => $heureSomme
         ]);
     }
 }
