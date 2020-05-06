58aa9d98c45261a6385a3e7cd53645392c17a69d
abenam27 <ga_benamara@esi.dz>
2020-05-06 20:33:01 +0200

    change name of instruction for debug !

--- parson/src/DataFixtures/ExerciseFixtures.php
+++ parson/src/DataFixtures/ExerciseFixtures.php
@@ -13,11 +13,17 @@ class ExerciseFixtures extends BaseFixture implements DependentFixtureInterface
     {
 
         $this->createMany(Exercise::class,20,function (Exercise $exercise,$i){
+            $instructions=array();
+            for ($j=0;$j<8;$j++)
+            {
+                array_push($instructions,"instruction $j");
+            }
+
             $exercise->setTitle($this->faker->sentence)
                      ->setMark($this->faker->randomFloat(2,0,5))
                      ->setType($this->faker->boolean(20) ? "normal" : "parson")
                      ->setQuestion($this->faker->sentence(8,true))
-                     ->setSolution($this->faker->sentences($this->faker->numberBetween(6,12),false))
+                     ->setSolution($instructions)
                      ->setCourse($this->getRandomRef(Course::class))
                      ->setCreatedAt($this->faker->dateTimeBetween('-1 months','-1 seconds'));
 
