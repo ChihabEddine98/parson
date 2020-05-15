36fc90bc985de56347acb6e5056085aae4c12945
abenam27 <ga_benamara@esi.dz>
2020-05-15 11:48:05 +0200

    add uploads dir

--- parson/src/Form/CourseType.php
+++ parson/src/Form/CourseType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Course;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,6 +18,7 @@ class CourseType extends AbstractType
             ->add('imgUrl')
             ->add('category')
             ->add('timeNeeded')
+            ->add('imgFile',FileType::class)
         ;
     }
 
